15ba515a84655fe6a6baaabf5a98ecd878eb287d
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-21 18:47:10 +0530

    [Darshan]: Add find max of 3 string

---
+++ GenericMaximum/MaximumOf3String.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMaximum
+{
+    internal class MaximumOf3String
+    { 
+        internal static void PrintMax(string[] array)
+        {
+            string max;
+            max = array[0].Length > array[1].Length ? (array[0].Length > array[2].Length ? array[0] : array[2]) : (array[1].Length > array[2].Length ? array[1] : array[2]);
+            Console.WriteLine("Largest strings from {0}, {1}, {2} is: {3}", array[0], array[1], array[2], max);
+        }
+    }
+}
--- GenericMaximum/Program.cs
+++ GenericMaximum/Program.cs
@@ -20,16 +20,31 @@
 
 
             //UC2
-            Console.WriteLine("Find Maximum of 3 float numbers");
-            float input;
-            float[] floatArray = new float[3];
+            //Console.WriteLine("Find Maximum of 3 float numbers");
+            //float input;
+            //float[] floatArray = new float[3];
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("Enter {0} number", i + 1);
+            //    input = float.Parse(Console.ReadLine());
+            //    floatArray[i] = input;
+            //}
+            //MaximumOfFloat.PrintMax(floatArray);
+
+
+            // UC3
+            Console.WriteLine("Find Maximum of String");
+            string input;
+            string[] stringArray = new string[3];
             for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine("Enter {0} number", i + 1);
-                input = float.Parse(Console.ReadLine());
-                floatArray[i] = input;
+                Console.WriteLine("Enter {0} string", i + 1);
+                input = Console.ReadLine();
+                stringArray[i] = input;
             }
-            MaximumOfFloat.PrintMax(floatArray);
+            MaximumOf3String.PrintMax(stringArray);
+
+
 
         }
     }
